c2005c326e4a65f1ff0d3f99c4cdfff24cd54090
16jovany2004 <16jovany2004@gmail.com>
2024-05-29 14:45:14 -0700

    updated Information

--- files/script.js
+++ files/script.js
@@ -1,3 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Hello, World!');
 });
+function startUp(){ //function is called when the website loads up or refreshes
+    ageCalculator()
+}
+function ageCalculator(){
+    //Calculates my age, so it is always current
+    const birthday = "2004-10-04"
+    const birthDate = new Date(birthday);
+    const currentDate = new Date();
+
+    let age = currentDate.getFullYear() - birthDate.getFullYear();
+    const monthDiff = currentDate.getMonth() - birthDate.getMonth();
+    if (monthDiff < 0 || (monthDiff === 0 && currentDate.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    document.getElementById("age").innerHTML = age;
+    // Age calculation part complete
+}
